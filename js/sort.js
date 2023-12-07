6e2b93741df504134f2e5af847e62caf1eb7883c
Nicolai Astrup Wiik <143996469+Nicolai-Astrup-Wiik@users.noreply.github.com>
2023-12-07 16:12:09 +0100

    not working

--- js/sort.js
+++ js/sort.js
@@ -1,30 +1,40 @@
 function sortAndDisplayVideos() {
 	const videoCards = Array.from(document.querySelectorAll('.video-card'));
 	const listItemsContainer = document.querySelector('.list-items-container');
-
 	const selectedButton = document.querySelector('.sort_button.selected');
 	const dataFilterBy = selectedButton.getAttribute('data-filter-by');
 
-	let sortedVideos = [...videoCards]; // Make a copy of the array to avoid mutating the original
+	const sortedVideos = videoCards.slice(); // Make a copy of the array
 
-	if (dataFilterBy === 'oldest') {
-		 sortedVideos.reverse();
-	}
+	sortedVideos.sort((a, b) => {
+		 const yearA = Number(a.getAttribute('data-year'));
+		 const monthA = Number(a.getAttribute('data-month'));
+		 const yearB = Number(b.getAttribute('data-year'));
+		 const monthB = Number(b.getAttribute('data-month'));
 
-	const container = document.createElement('div');
-	container.classList.add('list-item', 'visible');
+		 if (dataFilterBy === 'oldest') {
+			  if (yearA !== yearB) {
+					return yearA - yearB;
+			  } else {
+					return monthA - monthB;
+			  }
+		 } else if (dataFilterBy === 'newest') {
+			  if (yearA !== yearB) {
+					return yearB - yearA;
+			  } else {
+					return monthB - monthA;
+			  }
+		 }
+	});
+
+	listItemsContainer.innerHTML = ''; // Clear the container
 
 	sortedVideos.forEach(video => {
+		 const container = document.createElement('div');
+		 container.classList.add('list-item', 'visible');
 		 container.appendChild(video.cloneNode(true));
+		 listItemsContainer.appendChild(container);
 	});
-
-	// Remove existing list-items
-	document.querySelectorAll('.list-item').forEach(item => {
-		 listItemsContainer.removeChild(item);
-	});
-
-	// Append the new container with sorted video cards to the main list-items-container
-	listItemsContainer.appendChild(container);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -41,4 +51,6 @@ document.addEventListener('DOMContentLoaded', function () {
 			  sortAndDisplayVideos();
 		 });
 	});
+
+	sortAndDisplayVideos(); // Initially sort and display videos
 });
