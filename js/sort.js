168242d35b291160fa37f8b58b3c7d41c1f2f9c5
Nicolai Astrup Wiik <143996469+Nicolai-Astrup-Wiik@users.noreply.github.com>
2023-12-07 13:40:13 +0100

    add visible to the new list item and remove visible for existing ones

--- js/sort.js
+++ js/sort.js
@@ -1,41 +1,34 @@
-
-
-const videoCards = [...document.querySelectorAll('.video-card')];
+function sortAndDisplayVideos() {
+	const videoCards = Array.from(document.querySelectorAll('.video-card'));
 	const listItemsContainer = document.querySelector('.list-items-container');
 
 	const selectedButton = document.querySelector('.sort_button.selected');
 	const dataFilterBy = selectedButton.getAttribute('data-filter-by');
 
-function sortAndDisplayVideos() {
-	
+	let sortedVideos = [...videoCards]; // Make a copy of the array to avoid mutating the original
 
-	let sortedVideos = [...videoCards]; // remember make copy array
 	if (dataFilterBy === 'oldest') {
 		 sortedVideos.reverse();
 	}
 
-	//create new div with class list-item 
 	const container = document.createElement('div');
-	container.classList.add('list-item');
+	container.classList.add('list-item', 'visible');
 
-	//appnd a child for each 
 	sortedVideos.forEach(video => {
 		 container.appendChild(video.cloneNode(true));
 	});
 
-	// remove visible form all existing items 
+	// Remove existing list-items
 	document.querySelectorAll('.list-item').forEach(item => {
-		listItemsContainer.removeChild(item);
+		 listItemsContainer.removeChild(item);
 	});
-
-
 
 	// Append the new container with sorted video cards to the main list-items-container
 	listItemsContainer.appendChild(container);
 }
 
-
-
+document.addEventListener('DOMContentLoaded', function () {
+	const sortButtons = document.querySelectorAll('.sort_button');
 
 	sortButtons.forEach(button => {
 		 button.addEventListener('click', function () {
@@ -45,5 +38,7 @@ function sortAndDisplayVideos() {
 			  // Add 'selected' class to the clicked button
 			  button.classList.add('selected');
 
-			  sortAndDisplayVideos(); });
+			  sortAndDisplayVideos();
+		 });
 	});
+});
